0f81fee41639e717036a04d557a9fc92466b3410
Stephan Raabe <mail@stephan-raabe.de>
2021-11-30 10:57:02 +0000

    Update single-gpu-passthrough.sh

--- single-gpu-passthrough.sh
+++ single-gpu-passthrough.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/1)-Preparations
+# Set the parameter intel_iommu=on or amd_iommu=on respective to your system in the grub config
+# Set the parameter iommu=pt in grub config for safety reasons
+# You can add this parameter video=efifb:off fixes issues that few people have with returning back to the host. (Mostly AMD users)
+
+sudo vim /etc/default/grub
+
 printf "uncommend the the follow lines: # unix_sock_group = libvirt and # unix_sock_rw_perms = 0770"
 sudo vim /etc/libvirt/libvirtd.conf
 
