9a6baedd9e6bd4ceca8ac090279993f8a085aae0
Stephan Raabe <mail@stephan-raabe.de>
2021-11-30 09:50:18 +0000

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -2,6 +2,9 @@
 
 sudo pacman -S xfce4 xfce4-goodies chromium arc-gtk-theme arc-icon-theme
 
+# Add to .xinitrc
+# exec startxfce4 
+
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
