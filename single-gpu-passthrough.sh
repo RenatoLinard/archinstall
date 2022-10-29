2d857f8ed80ade639ec1afd70da3c78bf180cb79
Stephan Raabe <mail@stephan-raabe.de>
2022-10-29 14:44:32 +0000

    Update single-gpu-passthrough.sh

--- single-gpu-passthrough.sh
+++ single-gpu-passthrough.sh
@@ -2,23 +2,41 @@
 
 # https://gitlab.com/risingprismtv/single-gpu-passthrough/-/wikis/1)-Preparations
 # https://github.com/QaidVoid/Complete-Single-GPU-Passthrough
-# amd_iommu=on iommu=pt video=efifb:off
-# amd_iommu=on iommu=pt video=efifb:off vfio-pci.ids=1002:699f,1002:aae0 quiet
+
 # sudo vim /etc/default/grub
-# grub-mkconfig -o /boot/grub/grub.cfg
+# GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 amd_iommu=on iommu=pt pci=noats video=efifb:off"
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
 # reboot
 
-# Latest Kernel required
-# https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF
-# Ensure that the vfio-pci is loaded correctly
+# check that iommu groups are valid with iommu.sh
+sudo pacman -S virt-manager qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf
 
 printf "uncommend the the follow lines: # unix_sock_group = libvirt and # unix_sock_rw_perms = 0770"
 sudo vim /etc/libvirt/libvirtd.conf
+
+# add this at the bottom for log files
+# log_filters="1:qemu"
+# log_outputs="1:file:/var/log/libvirt/libvirtd.log"
 
 printf "edit the following lines user = 'raabe' and group = 'raabe'"
 sudo vim /etc/libvirt/qemu.conf
 
 sudo systemctl restart libvirtd
+
+# VGA bios was not required because of AMD GPU
+
+# Vendor-Reset required beause of AMD rest bug
+# git clone https://github.com/gnif/vendor-reset.git
+# sudo pacman -S git dkms base-devel linux-headers
+
+# sudo echo "vendor-reset" | sudo tee /etc/modules-load.d/vendor-reset.conf
+# sudo dkms install .
+# Add vendor reset to kernel modules (after btrf)
+# sudo vim /etc/mkinitcpio.conf
+# sudo mkinitcpio -p linux
+# check that vendor-reset is loaded
+# sudo dmesg | grep vendor_reset
+
 
 # Setup gpu.rom
 sudo mkdir /usr/share/vgabios
