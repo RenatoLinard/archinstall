14296eea1933a4016fd22a2e10a2d4ad83918323
Stephan Raabe <mail@stephan-raabe.de>
2023-02-16 10:13:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -148,18 +148,6 @@ vim /etc/mkinitcpio.conf
 echo "Start mkinitcpio -p linux"
 mkinitcpio -p linux
 
-# Install zram
-#yay -S zram-generator
-#sudo vim /etc/systemd/zram-generator.conf
-#Add
-#[zram0]
-#zram-size = ram / 2
-
-#sudo systemctl daemon-reload
-#sudo systemctl start /dev/zram0
-#reboot
-#free -h
-
 #exit
 echo "DONE! Please exit, umount -a & reboot"
 
