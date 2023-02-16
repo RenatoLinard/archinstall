3dee16c7cf726b667fea7d1bed70f5709b7df660
Stephan Raabe <mail@stephan-raabe.de>
2023-02-16 10:17:52 +0000

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -1,11 +1,15 @@
 # Install zram
-#yay -S zram-generator
-#sudo vim /etc/systemd/zram-generator.conf
-#Add
-#[zram0]
-#zram-size = ram / 2
+yay -S zram-generator
 
-#sudo systemctl daemon-reload
-#sudo systemctl start /dev/zram0
-#reboot
-#free -h
+# Edit zram-generator.conf
+echo "Manual step required!"
+echo "Add to /etc/systemd/zram-generator.conf (new file)" 
+echo "[zram0]"
+echo "zram-size = ram / 2"
+sudo vim /etc/systemd/zram-generator.conf
+
+# Restart daemon
+sudo systemctl daemon-reload
+sudo systemctl start /dev/zram0
+
+echo "DONE! Reboot now and check with free -h the ZRAM installation."
