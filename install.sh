90695615cd8b3bfc9b8f8daa5e0a0b1bfc8e9765
Stephan Raabe <mail@stephan-raabe.de>
2023-02-16 12:36:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,9 +35,13 @@
 # Start script
 # -----------------------------------------------------
 
-# Set drives
+# Set drives Laptop
 sda1="sda1"
 sda2="sda2"
+
+# Set drives Desktop
+# sda1="sda1"
+# sda2="sda2"
 
 # Confirm Start
 read -p "Do you want to start the installation?" c
@@ -45,16 +49,19 @@ read -p "Do you want to start the installation?" c
 # Sync time
 echo "Sync time"
 timedatectl set-ntp true
+echo "DONE."
 sleep 3
 
 # Reflector setup
 echo "Set refelctor"
 reflector -c Germany -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+echo "DONE."
 sleep 3
 
 # Pacman sync
 echo "Update packages"
 pacman -Syy
+echo "DONE."
 sleep 3
 
 # Confirm format of partitions
@@ -67,6 +74,7 @@ echo "Format partitions"
 mkfs.vfat -n BOOT /dev/$sda1
 mkfs.btrfs -L ROOT /dev/$sda2
 lsblk
+echo "DONE."
 
 # Confirm the creation of btrfs subvolumes
 read -p "Do you want to create the btrfs subvolumes?" c
@@ -82,6 +90,7 @@ btrfs su cr /mnt/@home
 btrfs su cr /mnt/@snapshots
 btrfs su cr /mnt/@log
 umount /mnt
+echo "DONE."
 
 # Confirm the mount of drives
 read -p "Do you want to mount all drives and subvolumnes?" c
@@ -97,6 +106,7 @@ mount -o compress=zstd:1,noatime,subvol=@log /dev/$sda2 /mnt/var/log
 mount -o compress=zstd:1,noatime,subvol=@snapshots /dev/$sda2 /mnt/.snapshots
 mount /dev/$sda1 /mnt/boot/efi
 lsblk
+echo "DONE."
 
 read -p "Do you want to install the base packages?" c
 
@@ -104,41 +114,43 @@ read -p "Do you want to install the base packages?" c
 echo "Install base packages"
 pacstrap -K /mnt base base-devel git linux linux-firmware vim openssh reflector rsync amd-ucode
 sleep 3
+echo "DONE."
 
 # Change to installed sytem
 read -p "Do you want to switch to the installation?" c
 echo "Change to installed system"
 arch-chroot /mnt
+echo "DONE."
 
 # Generate fstab
 read -p "Do you want to generate fstab?" c
 genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
+echo "DONE."
 
 # Confirm the cloning of archinstall script
 read -p "Do you want to clone the archinstall script?" c
 
 # Clone archinstall
 git clone https://gitlab.com/stephan.raabe/archinstall.git
+echo "DONE."
 
 # Start archinstall
 readm -p "Do you want to start archinstall?" c
 cd archinstall
 sh ./archinstall.sh
+echo "DONE."
 
-# ------------------------------------------------------
-# Install GRUB
-# ------------------------------------------------------
 # Confirm grub installation
 read -p "Do you want to clone the archinstall script?" c
 echo "Waiting 5 sec to start..."
 sleep 5
 
-echo "-> Install GRUB"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCHLINUX
 grub-mkconfig -o /boot/grub/grub.cfg
-echo "GRUB installed..."
+echo "DONE."
 sleep 3
+
 read -p "Do you want to continue?" c
 
 # Add btrfs to mkinitcpio
