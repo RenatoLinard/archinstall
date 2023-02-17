c040b005dde54b5e72f87211668ad36c7ba1094b
stephan.raabe <mail@stephan-raabe.de>
2023-02-17 14:05:28 +0100

    Updates

--- 1-install.sh
+++ 1-install.sh
@@ -63,14 +63,14 @@ read "Enter the name of the ROOT partition: " sda2
 # ------------------------------------------------------
 # Sync time
 # ------------------------------------------------------
-echo "Sync time"
+echo "-> Sync time"
 timedatectl set-ntp true
 echo "DONE."
 
 # ------------------------------------------------------
 # Reflector setup
 # ------------------------------------------------------
-echo "Set reflector"
+echo "-> Set reflector"
 reflector -c Germany -a 4 --sort rate --save /etc/pacman.d/mirrorlist
 echo "DONE."
 
@@ -82,7 +82,7 @@ read -p "Do you want to format the partitions?" c
 # ------------------------------------------------------
 # Format partitions
 # ------------------------------------------------------
-echo "Format partitions"
+echo "-> Format partitions"
 mkfs.fat -F 32 /dev/$sda1
 mkfs.btrfs -f /dev/$sda2
 lsblk
@@ -96,6 +96,7 @@ read -p "Do you want to create the btrfs subvolumes?" c
 # ------------------------------------------------------
 # Mount points for btrfs
 # ------------------------------------------------------
+echo "-> Create btrfs subvolumes"
 mount /dev/$sda2 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@cache
@@ -109,7 +110,7 @@ echo "DONE."
 # Confirm the mount of drives
 # ------------------------------------------------------
 read -p "Do you want to mount all drives and subvolumnes?" c
-
+echo "-> Mount all volumes"
 mount -o compress=zstd:1,noatime,subvol=@ /dev/$sda2 /mnt
 mkdir -p /mnt/{boot/efi,home,.snapshots,var/{cache,log}}
 mount -o compress=zstd:1,noatime,subvol=@cache /dev/$sda2 /mnt/var/cache
@@ -117,13 +118,14 @@ mount -o compress=zstd:1,noatime,subvol=@home /dev/$sda2 /mnt/home
 mount -o compress=zstd:1,noatime,subvol=@log /dev/$sda2 /mnt/var/log
 mount -o compress=zstd:1,noatime,subvol=@snapshots /dev/$sda2 /mnt/.snapshots
 mount /dev/$sda1 /mnt/boot/efi
-lsblk
 echo "DONE."
+lsblk
 
 # ------------------------------------------------------
 # Install base packages
 # ------------------------------------------------------
 read -p "Do you want to install the base packages?" c
+echo "-> Install base packages"
 pacstrap -K /mnt base base-devel git linux linux-firmware vim openssh reflector rsync amd-ucode
 echo "DONE."
 
@@ -131,6 +133,7 @@ echo "DONE."
 # Generate fstab
 # ------------------------------------------------------
 read -p "Do you want to generate fstab?" c
+echo "-> Generate fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 echo "DONE."
@@ -141,5 +144,5 @@ echo "DONE."
 mkdir /mnt/archinstall
 cp 2-archinstall.sh /mnt/archinstall
 cp 3-yay.sh /mnt/archinstall
-read -p "Do you want to chroot to the installation?" c
+read -p "Do you want to chroot to the installation and continue with 2-archinstall.sh to configure your arch installation?" c
 arch-chroot /mnt ./archinstall/2-archinstall.sh
