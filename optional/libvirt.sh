8524d1a224f5373933e2ce9b304ab5220dc53160
Stephan Raabe <mail@stephan-raabe.de>
2023-02-19 18:52:02 +0100

    Updates

--- optional/libvirt.sh
+++ optional/libvirt.sh
@@ -2,7 +2,6 @@
 
 # ------------------------------------------------------
 # Install Script for Libvirt
-# IMPORTANT: chmod +x libvirt.sh
 # ------------------------------------------------------
 
 read -p "Do you want to start? " s
@@ -15,20 +14,24 @@ echo "-> Install packages"
 sudo pacman -S virt-manager qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf
 echo "Packages installed..."
 
-echo "Manual Steps required:"
+# ------------------------------------------------------
+# Edit libvirtd.conf
+# ------------------------------------------------------
+echo "-> Manual Steps required:"
 echo "Open sudo vim /etc/libvirt/libvirtd.conf:"
 echo 'Remove # at the following lines: unix_sock_group = "libvirt" and unix_sock_rw_perms = "0770"'
 echo "Add the following two lines at the end of the file to enable logging:"
 echo 'log_filters="3:qemu 1:libvirt"'
 echo 'log_outputs="2:file:/var/log/libvirt/libvirtd.log"'
-read -p "Press any key to confirm: " c
+read -p "Press any key to open libvirtd.conf: " c
+sudo vim /etc/libvirt/libvirtd.conf
 
 # ------------------------------------------------------
 # Add user to the group
 # ------------------------------------------------------
 echo "-> Add user to groups"
 sudo usermod -a -G kvm,libvirt $(whoami)
-echo "User added to the groups..."
+echo "DONE."
 
 # ------------------------------------------------------
 # Enable services
@@ -37,25 +40,31 @@ echo "-> Enable services"
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
 echo "Services enabled..."
+echo "DONE."
 
+# ------------------------------------------------------
+# Edit qemu.conf
+# ------------------------------------------------------
 echo "Manual steps required:"
 echo "Open sudo vim /etc/libvirt/qemu.conf"
 echo "Uncomment and add your user name to user and group."
 echo 'user = "your username"'
 echo 'group = "your username"'
-read -p "Press any key to confirm: " c
+read -p "Press any key to open qemu.conf: " c
+sudo vim /etc/libvirt/qemu.conf
+
 # ------------------------------------------------------
 # Restart Services
 # ------------------------------------------------------
 echo "-> Restart services"
 sudo systemctl restart libvirtd
-echo "Services restarted..."
+echo "DONE."
 
 # ------------------------------------------------------
 # Autostart Network
 # ------------------------------------------------------
 echo "-> Set network to autostart"
 sudo virsh net-autostart default
-echo "Network set to autostart..."
+echo "DONE."
 
-echo "DONE! Restart recommended."
+echo "Please restart now with reboot."
--- optional/zram.sh
+++ optional/zram.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# -----------------------------------------------------
+# ZRAM Install Script
+# yay must be installed
+# -----------------------------------------------------
+
+read -p "Do you want to start now?" c
+
 # Install zram
 yay -S zram-generator
 
@@ -6,9 +15,13 @@ echo "Manual step required!"
 echo "Add to /etc/systemd/zram-generator.conf (new file)" 
 echo "[zram0]"
 echo "zram-size = ram / 2"
+
+# Open zram-generator.conf
+read -p "Open zram-generator.conf now? " c
 sudo vim /etc/systemd/zram-generator.conf
 
-# Restart daemon
+# Restart services
+read -p "Start systemctl services now? " c
 sudo systemctl daemon-reload
 sudo systemctl start /dev/zram0
 
