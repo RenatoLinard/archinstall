ffcfc47251c386609b8d61e95ea609494c9cff39
Stephan Raabe <mail@stephan-raabe.de>
2023-02-20 17:07:43 +0100

    Update

--- 2-archinstall.sh
+++ 2-archinstall.sh
@@ -116,7 +116,8 @@ echo "Services enabled"
 # ------------------------------------------------------
 # Confirm grub installation
 # ------------------------------------------------------
-read -p "-> Do you want to install grub now?" c
+# read -p "-> Do you want to install grub now?" c
+echo "-> Install GRUB Bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "DONE."
