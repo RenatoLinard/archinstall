ce9ae97e6f936f9173565114b16e34f6d8b59027
Stephan Raabe <mail@stephan-raabe.de>
2023-04-12 12:27:57 +0200

    Updates

--- 1-install.sh
+++ 1-install.sh
@@ -18,15 +18,33 @@
 # Confirm Start
 # ------------------------------------------------------
 clear
-echo "------------------------------------------------------"
-echo "Welcome to the Arch Install script"
-echo "------------------------------------------------------"
+echo "    _             _       ___           _        _ _ "
+echo "   / \   _ __ ___| |__   |_ _|_ __  ___| |_ __ _| | |"
+echo "  / _ \ | '__/ __| '_ \   | || '_ \/ __| __/ _' | | |"
+echo " / ___ \| | | (__| | | |  | || | | \__ \ || (_| | | |"
+echo "/_/   \_\_|  \___|_| |_| |___|_| |_|___/\__\__,_|_|_|"
+echo ""
+echo "by Stephan Raabe (2023)"
+echo "-----------------------------------------------------"
+echo ""
 echo "Important: Please make sure that you have followed the "
 echo "manual steps in the README to partition the harddisc!"
 echo "Warning: Run this script at your own risk."
 echo ""
-echo "DO YOU WANT TO START THE INSTALLATION NOW?"
-read -p "(Return to start, Ctrl+c to cancel)" c
+
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* ) 
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo ""
 
 # ------------------------------------------------------
 # Show partitions
